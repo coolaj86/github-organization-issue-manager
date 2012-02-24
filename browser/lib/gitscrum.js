7c9d77c6dffdd5992bf1d2acef9924a8a6f9dbbe
AJ ONeal <coolaj86@gmail.com>
2012-02-24 16:17:37 -0700

    got people selector working

--- browser/lib/gitscrum.js
+++ browser/lib/gitscrum.js
@@ -27,7 +27,7 @@
     , labelDirective = {
           ".js-label": {
               "label<-": {
-                  "@data-label-id": "label.name"
+                  "@data-label-name": "label.name"
                 , ".js-name": function (v) {
                     return v.item[0].name; //.name.replace('<', '&lt;');
                   }
@@ -40,7 +40,7 @@
     , repoDirective = {
           ".js-repo": {
               "repo<-": {
-                  "@data-label-id": "repo.name"
+                  "@data-repo-name": "repo.name"
                 , ".js-name": function (v) {
                     return v.item.name.replace('<', '&lt;');
                   }
@@ -50,10 +50,9 @@
     , assigneeDirective = {
           ".js-assignee": {
               "assignee<-": {
-                  "@data-assignee-id": "assignee.name"
-                , ".js-name": function (v) {
-                    return v.item.name.replace('<', '&lt;');
-                  }
+                  "@data-assignee-login": "assignee.login"
+                , ".js-login": "assignee.login"
+                , ".js-photo@src": "assignee.avatar_url"
               }
           }
       }
@@ -62,21 +61,31 @@
     , issueTplFn
     , labelTplFn
     , repoTplFn
+    , assigneeTplFn
     ;
 
   gitscrum.repos = [];
   gitscrum.issues = [];
   gitscrum.labels = [];
+  gitscrum.assignees = [];
 
   function templateIssues() {
     var labelMap = {}
       , labelReduced = {}
+      , assigneeMap = {}
       ;
 
     gitscrum.issues.forEach(function (issue) {
+      if (issue.assignee) {
+        assigneeMap[issue.assignee.login] = issue.assignee;
+      }
       issue.labels.forEach(function (label) {
         labelMap[label.url.replace(/.*\/repos\//, '')] = label;
       });
+    });
+
+    Object.keys(assigneeMap).forEach(function (key) {
+      gitscrum.assignees.push(assigneeMap[key]);
     });
 
     Object.keys(labelMap).forEach(function (key) {
@@ -95,6 +104,7 @@
     pure("#js-issues").render(gitscrum.issues, issueTplFn);
     pure("#js-labels").render(gitscrum.labels, labelTplFn);
     pure("#js-repos").render(gitscrum.repos, repoTplFn);
+    pure("#js-assignees").render(gitscrum.assignees, assigneeTplFn);
   }
 
   function createOrgGetter(thingyName, thingyArr) {
@@ -215,18 +225,117 @@
     $('.js-issue').show();
     filterBySearch();
     filterByLabel();
+    filterByRepo();
+    filterByAssignee();
+  }
+
+  function selectKeyword() {
+    filterAll();
+  }
+
+  function escapeRegExp(str) {
+    return str.replace(/[-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
   }
 
   function filterBySearch() {
-    if (true /* no search term given */) {
-      gitscrum.issues.forEach(function (issue) {
-        //console.log('filterBySearch');
-        //console.log(issue.closed_at);
-        if (issue.closed_at) {
-          $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
-        }
-      });
+    var keyword = $('#js-search-issues .js-keyword').val().trim()
+      ;
+
+    function hideClosed(issue) {
+      if (issue.closed_at) {
+        $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
+      }
     }
+
+    function matchesKeyword(issue) {
+      if (!keyword.exec(issue.title) && !keyword.exec(issue.body)) {
+        $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
+      }
+    }
+
+    if (!keyword) {
+      gitscrum.issues.forEach(hideClosed);
+      return;
+    }
+
+    if ('/' === keyword[0] && '/' === keyword[keyword.length - 1]) {
+      keyword = keyword.substr(1, keyword.length - 2);
+    } else {
+      keyword = escapeRegExp(keyword);
+    }
+    keyword = new RegExp(keyword, 'i');
+
+    gitscrum.issues.forEach(matchesKeyword);
+  }
+
+  function filterByAssignee() {
+    var assignees = []
+      , selectedAssignees = $('.js-assignee[data-selected="selected"]')
+      ;
+
+    if (!selectedAssignees.length) {
+      return;
+    }
+
+    selectedAssignees.forEach(function (el) {
+      var login = $(el)[0].dataset.assigneeLogin;
+      // TODO fix the bug causing the assignee name to be missing
+      login = login || $(el).find('.js-login').text();
+      assignees.push(login);
+    });
+
+    console.log('filterByAssigneeNames:', assignees);
+    gitscrum.issues.forEach(function (issue) {
+      var hasAssignee
+        ;
+
+      console.log(issue);
+      hasAssignee = assignees.some(function (name) {
+        var assignee = issue.assignee
+          ;
+        
+        return assignee && (-1 !== assignees.indexOf(assignee.login));
+      });
+
+      if (!hasAssignee) {
+        $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
+      }
+    });
+  }
+
+  function filterByRepo() {
+    var repos = []
+      , selectedRepos = $('.js-repo[data-selected="selected"]')
+      ;
+
+    if (!selectedRepos.length) {
+      return;
+    }
+
+    selectedRepos.forEach(function (el) {
+      var name = $(el)[0].dataset.repoName;
+      // TODO fix the bug causing the repo name to be missing
+      name = name || $(el).find('.js-name').text();
+      repos.push(name);
+    });
+
+    console.log('filterByRepoNames:', repos);
+    gitscrum.issues.forEach(function (issue) {
+      var hasRepo
+        ;
+
+      console.log(issue);
+      hasRepo = repos.some(function (name) {
+        var repo = issue.url.match(/https:\/\/api\.github\.com\/repos\/(.*)\/(.*)\/issues\/(\d+)/)[2]
+          ;
+        
+        return -1 !== repos.indexOf(repo);
+      });
+
+      if (!hasRepo) {
+        $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
+      }
+    });
   }
 
   function filterByLabel() {
@@ -280,6 +389,44 @@
     filterAll();
   }
 
+  function selectAssignee() {
+    var assignee = $(this)
+      ;
+
+    if (!assignee[0].dataset.selected) {
+      assignee.css('background-color', '#ffef74');
+      assignee.css('border-color', '');
+      assignee[0].dataset.selected = 'selected';
+      assignee.find('.js-unselect').show();
+    } else {
+      assignee.css('border-color', '#ffef74');
+      assignee.css('background-color', null);
+      assignee[0].dataset.selected = '';
+      assignee.find('.js-unselect').hide();
+    }
+
+    filterAll();
+  }
+
+  function selectRepo() {
+    var repo = $(this)
+      ;
+
+    if (!repo[0].dataset.selected) {
+      repo.css('background-color', 'gray');
+      repo.css('border-color', '');
+      repo[0].dataset.selected = 'selected';
+      repo.find('.js-unselect').show();
+    } else {
+      repo.css('border-color', '');
+      repo.css('background-color', null);
+      repo[0].dataset.selected = '';
+      repo.find('.js-unselect').hide();
+    }
+
+    filterAll();
+  }
+
   function attachHandlers() {
     console.log('filling in the blanks');
     $('#js-login .js-user').val(store.get('user'))
@@ -288,11 +435,19 @@
     console.log('filled in the blanks');
 
     issueTplFn = pure("#js-issues").compile(issueDirective);
+    $('#js-issues').html('');
     labelTplFn = pure("#js-labels").compile(labelDirective);
+    $('#js-labels').html('');
     repoTplFn = pure("#js-repos").compile(repoDirective);
+    $('#js-repos').html('');
+    assigneeTplFn = pure("#js-assignees").compile(assigneeDirective);
+    $('#js-assignees').html('');
 
     $('body').delegate('.js-label', 'click', selectLabel);
+    $('body').delegate('.js-repo', 'click', selectRepo);
+    $('body').delegate('.js-assignee', 'click', selectAssignee);
     $('body').delegate('.js-issue', 'click', showBody);
+    $('body').delegate('#js-search-issues .js-keyword', 'keyup', selectKeyword);
     $('body').delegate('form#js-login', 'submit', function (ev) {
       ev.preventDefault();
 
