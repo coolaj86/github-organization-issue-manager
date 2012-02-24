2fd84cda31b5ea6f0a6e74f65c5c5bbb85a82d1e
AJ ONeal <coolaj86@gmail.com>
2012-02-24 14:26:15 -0700

    working label selectors

--- browser/lib/gitscrum.js
+++ browser/lib/gitscrum.js
@@ -1,14 +1,15 @@
 (function () {
   "use strict";
 
-  var request = require('ahr2')
+  var gitscrum = module.exports
+    , request = require('ahr2')
     , $ = require('ender')
     , localStorage = require('localStorage')
     , JsonStorage = require('json-storage')
     , store = JsonStorage(localStorage, 'gitscrum')
     , forEachAsync = require('forEachAsync')
     , pure = require('pure').$p
-    , directive = {
+    , issueDirective = {
           ".js-issue": {
               "issue<-": {
                   "@data-issue-id": "issue.id"
@@ -23,41 +24,120 @@
               }
           }
       }
+    , labelDirective = {
+          ".js-label": {
+              "label<-": {
+                  "@data-label-id": "label.name"
+                , ".js-name": function (v) {
+                    return v.item[0].name; //.name.replace('<', '&lt;');
+                  }
+                , "@style": function (v) {
+                    return 'border-color: #' + v.item[0].color + ';';
+                  }
+              }
+          }
+      }
+    , repoDirective = {
+          ".js-repo": {
+              "repo<-": {
+                  "@data-label-id": "repo.name"
+                , ".js-name": function (v) {
+                    return v.item.name.replace('<', '&lt;');
+                  }
+              }
+          }
+      }
+    , assigneeDirective = {
+          ".js-assignee": {
+              "assignee<-": {
+                  "@data-assignee-id": "assignee.name"
+                , ".js-name": function (v) {
+                    return v.item.name.replace('<', '&lt;');
+                  }
+              }
+          }
+      }
     , auth
     , githubOrg
-    , repos
-    , allIssues = []
-    , tfn
+    , issueTplFn
+    , labelTplFn
+    , repoTplFn
     ;
 
-  function templateIssues() {
-    pure("#js-issues").render(allIssues, tfn);
-  }
+  gitscrum.repos = [];
+  gitscrum.issues = [];
+  gitscrum.labels = [];
 
-  function getGithubRepoIssues(next, repo) {
-    var url = "/repos/" + githubOrg + "/" + repo.name + "/issues?per_page=100"
-      , issues = store.get(url)
+  function templateIssues() {
+    var labelMap = {}
+      , labelReduced = {}
       ;
 
-    if (issues) {
-      allIssues = allIssues.concat(issues);
-      next();
-      return;
+    gitscrum.issues.forEach(function (issue) {
+      issue.labels.forEach(function (label) {
+        labelMap[label.url.replace(/.*\/repos\//, '')] = label;
+      });
+    });
+
+    Object.keys(labelMap).forEach(function (key) {
+      var val = labelMap[key]
+        , name = val.name
+        , label = labelReduced[name] || []
+        ;
+
+      labelReduced[name] = label;
+      label.push(val);
+    });
+    Object.keys(labelReduced).forEach(function (key) {
+      gitscrum.labels.push(labelReduced[key]);
+    });
+
+    pure("#js-issues").render(gitscrum.issues, issueTplFn);
+    pure("#js-labels").render(gitscrum.labels, labelTplFn);
+    pure("#js-repos").render(gitscrum.repos, repoTplFn);
+  }
+
+  function createOrgGetter(thingyName, thingyArr) {
+    function getGithubRepoThings(next, repo) {
+      var url = "/repos/" + githubOrg + "/" + repo.name + "/" + thingyName + "?per_page=100"
+        , storedArr = store.get(url)
+        ;
+
+      // TODO needs expiry time
+      if (storedArr) {
+        storedArr.forEach(function (item) {
+          thingyArr.push(item);
+        });
+        next();
+        return;
+      }
+
+      function eatThingyArr(err, ahr, data) {
+        if (err) {
+          console.error(url);
+          console.error(err);
+        }
+        console.log(thingyName + ' for ' + repo.name + ':');
+        // preserving the original ref
+        data.forEach(function (datum) {
+          thingyArr.push(datum);
+        });
+        store.set(url, data);
+        next();
+      }
+
+      request.get("https://" + auth  + "@api.github.com" + url).when(eatThingyArr);
     }
 
-    function eatIssues(err, ahr, data) {
-      console.log('issues for ' + repo.name + ':');
-      console.log(data);
-      allIssues = allIssues.concat(data);
-      store.set(url, data);
-      next();
-    }
+    return getGithubRepoThings;
+  }
 
-    request.get("https://" + auth  + "@api.github.com" + url).when(eatIssues);
+  function getGithubOrgLabels() {
+    // forEachAsync(gitscrum.repos, getGithubRepoIssues).then(templateIssues);
   }
 
   function getGithubOrgIssues() {
-    forEachAsync(repos, getGithubRepoIssues).then(templateIssues);
+    forEachAsync(gitscrum.repos, createOrgGetter('issues', gitscrum.issues)).then(templateIssues);
   }
 
   /*
@@ -98,21 +178,21 @@
       store.set('pass', pass);
       store.set('org', githubOrg);
 
-      repos = data;
-      store.set(url, repos);
+      gitscrum.repos = data;
+      store.set(url, gitscrum.repos);
 
       getGithubOrgIssues();
     }
 
-    repos = store.get(url);
+    gitscrum.repos = store.get(url);
 
-    /*
-    if (repos) {
-      console.log(repos);
+    ///*
+    if (gitscrum.repos && gitscrum.repos.length) {
+      console.log(gitscrum.repos);
       getGithubOrgIssues();
       return;
     }
-    */
+    //*/
 
     auth = user + ':' + pass;
 
@@ -131,69 +211,100 @@
     $("[data-issue-id=" + id + "]").find('.js-body').toggleClass('hidden');
   }
 
-  function onDragStart() {
-    $(this).addClass('drag-start');
-    console.log('dragstart', this);
+  function filterAll() {
+    $('.js-issue').show();
+    filterBySearch();
+    filterByLabel();
   }
 
-  function onDragEnd() {
-    $(this).removeClass('drag-start');
-    console.log('dragend', this);
+  function filterBySearch() {
+    if (true /* no search term given */) {
+      gitscrum.issues.forEach(function (issue) {
+        //console.log('filterBySearch');
+        //console.log(issue.closed_at);
+        if (issue.closed_at) {
+          $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
+        }
+      });
+    }
   }
 
-  function onDragOver(ev) {
-    ev.preventDefault(); // Necessary. Allows us to drop.
-    //ev.stopPropagation();
+  function filterByLabel() {
+    var labels = []
+      , selectedLabels = $('.js-label[data-selected="selected"]')
+      ;
 
-    ev.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    if (!selectedLabels.length) {
+      return;
+    }
 
-    console.log('dragOver', this);
-    return false;
+    selectedLabels.forEach(function (el) {
+      var name = $(el)[0].dataset.labelName;
+      // TODO fix the bug causing the label name to be missing
+      name = name || $(el).find('.js-name').text();
+      // we know we don't have duplicate labels
+      console.log(name);
+      labels.push(name);
+    });
+
+    gitscrum.issues.forEach(function (issue) {
+      var hasLabel
+        ;
+
+      hasLabel = issue.labels.some(function (label) {
+        return -1 !== labels.indexOf(label.name);
+      });
+
+      // var id = $('.js-issue')[3].dataset.issueId
+      // $('.js-issue[data-issue-id="' + id + '"]').hide();
+      if (!hasLabel) {
+        $('.js-issue[data-issue-id="' + issue.id + '"]').hide();
+      }
+    });
   }
 
-  function handleDragEnter(e) {
-    // this / e.target is the current hover target.
-    $(this).addClass('drag-enter');
-    console.log('dragEnter', this);
-  }
+  function selectLabel() {
+    var label = $(this);
+    if (!label[0].dataset.selected) {
+      label.css('background-color', label.css('border-color'));
+      label.css('border-color', '');
+      label[0].dataset.selected = 'selected';
+      label.find('.js-unselect').show();
+    } else {
+      label.css('border-color', label.css('background-color'));
+      label.css('background-color', '');
+      label[0].dataset.selected = '';
+      label.find('.js-unselect').hide();
+    }
 
-  function handleDragLeave(e) {
-    // this / e.target is previous target element.
-    $(this).removeClass('drag-enter');
-    console.log('dragLeave', this);
-  }
-
-  function onDragOther() {
-    // ignore
-    console.log('dragOther', this);
-  }
-
-  function onDrop() {
+    filterAll();
   }
 
   function attachHandlers() {
+    console.log('filling in the blanks');
     $('#js-login .js-user').val(store.get('user'))
     $('#js-login .js-pass').val(store.get('pass'));
     $('#js-login .js-org').val(store.get('org'))
+    console.log('filled in the blanks');
 
-    tfn = pure("#js-issues").compile(directive);
+    issueTplFn = pure("#js-issues").compile(issueDirective);
+    labelTplFn = pure("#js-labels").compile(labelDirective);
+    repoTplFn = pure("#js-repos").compile(repoDirective);
 
-/*
-    $('body').delegate('.js-issue', 'dragstart', onDragStart);
-    $('body').delegate('.js-issue', 'dragenter', onDragOther);
-    $('body').delegate('.js-issue', 'dragover', onDragOther);
-    $('body').delegate('.js-issue', 'dragleave', onDragOther);
-    $('body').delegate('.js-issue', 'dragend', onDragEnd);
-    $('body').delegate('.js-issue', 'drop', onDrop);
-*/
-//    $('body').delegate('.js-issue', 'click', showBody);
+    $('body').delegate('.js-label', 'click', selectLabel);
+    $('body').delegate('.js-issue', 'click', showBody);
     $('body').delegate('form#js-login', 'submit', function (ev) {
       ev.preventDefault();
+
+      var user = $('#js-login .js-user').val()
+        , pass = $('#js-login .js-pass').val()
+        ;
+
       githubOrg = $('#js-login .js-org').val()
-      getGithubOrgRepos(
-          $('#js-login .js-user').val()
-        , $('#js-login .js-pass').val()
-      );
+
+      request.get("https://" + user  + ":" + pass + "@api.github.com/user").when(function () {
+        getGithubOrgRepos(user, pass);
+      });
     });
   }
 
--- browser/lib/index.js
+++ browser/lib/index.js
@@ -14,9 +14,17 @@ function attachHandlers() {
 
     // this/e.target is the source node.
     $(this).addClass('moving');
+    var self = this;
+    setTimeout(function () {
+      //$(self).addClass('moving');
+      //$(self).addClass('xmoving');
+    }, 412);
+    //$(this).appendTo('#graveyard');
   };
 
   function handleDragOver(e) {
+    // e.offset === undefined
+    console.log('dragover', $(this).offset(), $(dragSrcEl_).offset(), $(e).offset(), e);
     if (e.preventDefault) {
       e.preventDefault(); // Allows us to drop.
     }
@@ -27,42 +35,63 @@ function attachHandlers() {
   };
 
   function handleDragEnter(e) {
+    console.log('dragenter');
     $(this).addClass('over');
   };
 
   function handleDragLeave(e) {
+    console.log('dragleave');
     // this/e.target is previous target element.
     $(this).removeClass('over');
   };
 
   function handleDrop(e) {
+    console.log('drop');
     e.stopPropagation(); // stops the browser from redirecting.
 
     // Don't do anything if we're dropping on the same js-issue we're dragging.
-    if (dragSrcEl_ != this) {
-      dragSrcEl_.innerHTML = $(this).html();
-      $(this).html(e.dataTransfer.getData('text/html'));
-
-      // Set number of times the js-issue has been moved.
-      var count = $(this).find('.count');
-      var newCount = parseInt(count.attr('data-col-moves')) + 1;
-      count.attr('data-col-moves', newCount);
-      count.text('moves: ' + newCount);
+    if (dragSrcEl_ === this) {
+      return false;
     }
+
+    $(dragSrcEl_).remove();
+    $(this).after(dragSrcEl_);
+    $(this).removeClass('moving');
+    $(this).removeClass('xmoving');
+
+    setTimeout(function () {
+      $('.js-issue').removeClass('xmoving');
+      $('.js-issue').removeClass('moving');
+    }, 200);
+    return false;
+
+    //$(dragSrcEl_).html($(this).html());
+    //$(this).html(e.dataTransfer.getData('text/html'));
+
+    // Set number of times the js-issue has been moved.
+    var count = $(this).find('.count');
+    var newCount = parseInt(count.attr('data-col-moves')) + 1;
+    count.attr('data-col-moves', newCount);
+    count.text('moves: ' + newCount);
 
     return false;
   };
 
   function handleDragEnd(e) {
+    console.log('dragend');
     $('.js-issue').removeClass('over');
     $('.js-issue').removeClass('moving');
+    $('.js-issue').removeClass('xmoving');
+    console.log('dragend');
   };
 
+  /*
   var body = document.body.innerHTML;
   document.body.innerHTML = '';
   setTimeout(function () {
     document.body.innerHTML = body;
   }, 1 * 1000);
+  */
 
   $('body').delegate('.js-issue', 'dragstart', handleDragStart);
   $('body').delegate('.js-issue', 'dragend', handleDragEnd);
